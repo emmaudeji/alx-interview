7714f7986317bf7c001779673839ddc40cb5f325
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-06-30 17:01:04 +0100

    test file

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
