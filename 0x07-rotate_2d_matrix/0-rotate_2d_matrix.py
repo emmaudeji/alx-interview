a0c49af8c64d498a8afc0cbdab118218da971a89
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-08-03 19:17:39 +0100

    rotates 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''scripts to rotate nxn 2D matrix clockwise
+return: Nothing '''
+
+
+def rotate_2d_matrix(matrix):
+  # step-1: transpose the matrix
+  for i in range(len(matrix)):
+    for j in range(i, len(matrix)):
+      matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+    
+  # step-2: reverse the matrix
+  N=len(matrix)
+  for i in range(N//2):
+    for j in range(N):
+      matrix[j][i],matrix[j][N-1-i]=matrix[j][N-1-i],matrix[j][i]
+  
