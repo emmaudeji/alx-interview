ff9cbb2061f20ed40ade4ce976ea32c8e71f9926
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-08-10 14:07:28 +0100

    solution-make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,44 +1,29 @@
 #!/usr/bin/python3
-""" 
-Making Change
-"""
+'''Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet
+    a given amount total.
+'''
+import sys
 
 
 def makeChange(coins, total):
-    """
-    Return the minimum number of coins needed to meet a given total
-    Args:
-        coins (list of ints): a list of coins of different values
-        total (int): total value to be met
-    Return:
-        Number of coins or 
-        -1 if meeting the total is not possible
-    """
+    '''
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
     if total <= 0:
         return 0
-    if coins == [] or coins is None:
-        return -1
-    try:
-        n = coins.index(total)
-        return 1
-    except ValueError:
-        pass
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
 
-    coins.sort(reverse=True)
-    coin_count = 0
-    for i in coins:
-        if total % i == 0:
-            coin_count += int(total / i)
-            return coin_count
-        if total - i >= 0:
-            if int(total / i) > 1:
-                coin_count += int(total / i)
-                total = total % i
-            else:
-                coin_count += 1
-                total -= i
-                if total == 0:
-                    break
-    if total > 0:
+    if table[total] == sys.maxsize:
         return -1
-    return coin_count
+    return table[total]
